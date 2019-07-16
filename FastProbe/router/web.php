b668e131c522704820a7413452cfee671fefcd50
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-07-16 19:20:24 +0800

    测试路由文件

---
+++ FastProbe/router/web.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ProjectName: FastProbe-web.
+ * Author: SaigyoujiYuyuko
+ * QQ: 3558168775
+ * Date: 2019/7/16
+ * Time: 13:42
+ *
+ * Copyright © 2019 SaigyoujiYuyuko. All rights reserved.
+ */
+
+$router->get("/", function (){
+    echo "qaq";
+});
