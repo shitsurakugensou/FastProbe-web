5f298e0c368c040fd53e16d4b61603403c49ce84
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-07-17 16:33:00 +0800

    主页视图

---
+++ FastProbe/view/serverlist.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+
+        <title><?php echo getenv("WEB_TITLE"); ?></title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <link href="https://cdn.staticfile.org/mdui/0.4.2/css/mdui.css" rel="stylesheet">
+        <link href="https://cdn.staticfile.org/animate.css/3.7.0/animate.css" rel="stylesheet">
+        <link href="https://cdn.staticfile.org/font-awesome/5.8.0/css/fontawesome.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC:400|Quicksand:400" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
